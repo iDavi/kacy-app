faa827683adc4275fbafc93fbfcf9fa05a7e42f7
iDavi <odavi20527@gmail.com>
2025-01-01 11:45:17 -0300

    (feat) add buttons row in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { WebsiteTitleComponent } from "./website-title/website-title.component";
+import { PrimaryButtonComponent } from "../shared/button/primary-button/primary-button.component";
+import { ButtonComponent } from "../shared/button/button.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WebsiteTitleComponent],
+  imports: [WebsiteTitleComponent, ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
