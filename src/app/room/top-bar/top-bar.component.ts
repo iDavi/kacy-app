0449879696b13b46cda2a34a5023e9c05064dc99
iDavi <odavi20527@gmail.com>
2025-01-05 23:29:23 -0300

    (feat) add copy room id button functionality

--- src/app/room/top-bar/top-bar.component.ts
+++ src/app/room/top-bar/top-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ButtonComponent } from "../../shared/components/button/button.component";
+import { ClipboardService } from 'ngx-clipboard';
 
 @Component({
   selector: 'app-top-bar',
@@ -10,4 +11,13 @@ import { ButtonComponent } from "../../shared/components/button/button.component
 })
 export class TopBarComponent {
   @Input() roomId : string = "";
+
+  constructor(private _clipboardService : ClipboardService) {
+
+  }
+
+  copyId() {
+    this._clipboardService.copy(this.roomId)
+    alert("ID copied to clipboard")
+  }
 }
