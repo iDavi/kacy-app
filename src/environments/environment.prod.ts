20f685d442ed591d5ee13d662c72e7769a63f043
iDavi <odavi20527@gmail.com>
2025-01-05 22:19:03 -0300

    (fix) fix websocket url env variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    websocketUrl: 'ws://localhost:3000/socket/websocket' 
+    websocketUrl: 'ws://kacychat.site/socket' 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    websocketUrl: 'ws://localhost:4000/socket' 
+    websocketUrl: 'ws://localhost:3000/socket' 
 };
